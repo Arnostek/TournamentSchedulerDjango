951e3069a2a882eec72ad4af74de05bca53b0644
Arnost <arnost.chaloupka@zeppelin.com>
2019-03-24 02:07:01 +0100

    prvni nastrel databazoveho modelu

--- django/tournament/models.py
+++ django/tournament/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Tournament(models.Model):
+    name = models.CharField(max_length = 200)
+    slug = models.SlugField()
+
+class Division(models.Model):
+    name = models.CharField(max_length = 200)
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
+
+class Group(models.Model):
+    name = models.CharField(max_length = 200)
+    tournament = models.ForeignKey(Division, on_delete=models.CASCADE)
+    finished = models.BooleanField(default=False)
+
+class Team(models.Model):
+    name = models.CharField(max_length = 200)
+
+class Match(models.Model):
+    home = models.ForeignKey(Team, related_name = 'home_matches', on_delete=models.CASCADE)
+    away = models.ForeignKey(Team, related_name = 'away_matches', on_delete=models.CASCADE)
+    referee = models.ForeignKey(Team, related_name = 'referee_matches', on_delete=models.CASCADE)
