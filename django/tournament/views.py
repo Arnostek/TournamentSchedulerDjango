3b3f7c969660a83aa35454f1420bb33a0a591f22
Arnost <arnost.chaloupka@zeppelin.com>
2019-03-24 02:08:09 +0100

    seznam turnaju

--- django/tournament/views.py
+++ django/tournament/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Tournament
 
 # Create your views here.
+
+class TournamentListView(ListView):
+    template_name = 'tournament-list.html'
+    queryset = Tournament.objects.all()
+    context_object_name = 'tournaments'
