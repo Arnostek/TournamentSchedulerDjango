5a4642161728a0c0503db3a48a1a70256d195c91
Arnost <arnost.chaloupka@zeppelin.com>
2019-03-26 03:26:58 +0100

    vytvoreni zaznamu seed po zalozeni divize

--- django/tournament/models.py
+++ django/tournament/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.dispatch import receiver
 
 #
 class Tournament(models.Model):
@@ -8,7 +9,25 @@ class Tournament(models.Model):
 class Division(models.Model):
     name = models.CharField(max_length = 200)
     slug = models.SlugField()
+    teams = models.PositiveSmallIntegerField()
     tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
+
+    def CreateSeed(self):
+
+        # create SeedDivision
+        for rank in range(self.teams):
+            rank = rank + 1
+            # pripravim si TeamPlaceholder
+            tph = TeamPlaceholder(name = "{}_SEED#{:02d}".format(self.slug, rank))
+            tph.save()
+            # zalozim DivisionSeed
+            seed = self.divisionseed_set.create(rank = rank, teamPlaceholder = tph)
+
+
+@receiver(models.signals.post_save, sender=Division)
+def division__after_create(sender, instance, created, *args, **kwargs):
+    if created:
+        instance.CreateSeed()
 
 class Group(models.Model):
     name = models.CharField(max_length = 200)
