976d4ca155befa1d68d0726f3a282e9c36d69fe5
Arnost <arnost.chaloupka@zeppelin.com>
2019-03-26 03:27:18 +0100

    zalozeni testovacich dat

---
+++ django/tournament/TestData.py
@@ -0,0 +1,47 @@
+from . import models
+
+# run it in shell by:
+# from tournament.TestData import TestData
+
+# zakladani testovacich dat
+class TestData:
+
+    def __init__(self, tournamentName, tournamentSlug):
+
+        self.t = models.Tournament()
+        self.t.name = tournamentName
+        self.t.slug = tournamentSlug
+        self.t.save()
+
+    def AddDivision(self, name, slug, teams_count):
+
+        self.actual_division = self.t.division_set.create(name = name, slug = slug, teams = teams_count)
+
+    def SeedDivision(self,teams):
+
+        i = 0
+
+        for team_name in teams:
+            # najdu TeamPlaceholder
+            tph = self.actual_division.divisionseed_set.all()[i].teamPlaceholder
+            # vytvorim tym
+            team = models.Team(name = team_name)
+            team.save()
+            # ulozim tym do tph
+            tph.team = team
+            tph.save()
+
+            i += 1
+
+#    def AddGroups
+
+
+
+# turnaj
+tdata = TestData("Prague 2019", "PRG2019")
+tdata.AddDivision('Men A','MenA',12)
+tdata.SeedDivision('Prague A, Prague B, Vidra'.split(','))
+tdata.AddDivision('Men B','MenA',16)
+tdata.AddDivision('Ladies','Ladies',10)
+tdata.AddDivision('U14','U14',6)
+tdata.AddDivision('U16','U16',7)
