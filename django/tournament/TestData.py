0047f7a670e470fd49927c60171d1e9982478c8b
Arnost <arnost.chaloupka@zeppelin.com>
2019-03-27 00:34:20 +0100

    system pro Ladies

--- django/tournament/TestData.py
+++ django/tournament/TestData.py
@@ -39,6 +39,7 @@ class TestData:
 
 # turnaj
 tdata = TestData("Prague 2019", "PRG2019")
+####################################################
 # men A
 tdata.AddDivision('Men A','MenA',12)
 # zakladani skupiny
@@ -50,23 +51,33 @@ for misto in [11,9,7,5]:
     tdata.actual_division.CreateGroups(['{}th'.format(misto)], tdata.actual_division.GetGroupsRanks(['A','B'])[misto - 1: misto + 1])
 # 3rd
 tdata.actual_division.CreateGroups(['3rd'], tdata.actual_division.GetGroupsRanks(['S1','S2'])[2:4])
-# 3rd
+# final
 tdata.actual_division.CreateGroups(['final'], tdata.actual_division.GetGroupsRanks(['S1','S2'])[0:2])
 
-
-
+####################################################
 # men B
 tdata.AddDivision('Men B','MenB',16)
 tdata.actual_division.CreateGroups(['A','B','C','D'], tdata.actual_division.seed_placeholders)
 
+####################################################
 # Ladies
 tdata.AddDivision('Ladies','Ladies',10)
 tdata.actual_division.CreateGroups(['A','B'], tdata.actual_division.seed_placeholders)
+tdata.actual_division.CreateGroups(['S1','S2'], tdata.actual_division.GetGroupsRanks(['A','B'])[:4])
+# zapasy o mista bez semi
+for misto in [9,7,5]:
+    tdata.actual_division.CreateGroups(['{}th'.format(misto)], tdata.actual_division.GetGroupsRanks(['A','B'])[misto - 1: misto + 1])
+# 3rd
+tdata.actual_division.CreateGroups(['3rd'], tdata.actual_division.GetGroupsRanks(['S1','S2'])[2:4])
+# final
+tdata.actual_division.CreateGroups(['final'], tdata.actual_division.GetGroupsRanks(['S1','S2'])[0:2])
 
+####################################################
 # U14
 tdata.AddDivision('U14','U14',6)
 tdata.actual_division.CreateGroups(['A','B'], tdata.actual_division.seed_placeholders)
 
+####################################################
 # U16
 tdata.AddDivision('U16','U16',7)
 tdata.actual_division.CreateGroups(['A'], tdata.actual_division.seed_placeholders)
