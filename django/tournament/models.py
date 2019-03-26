e0fddf57987cbc8141f1c1ddbf0fefab8792cf78
Arnost <arnost.chaloupka@zeppelin.com>
2019-03-26 01:47:30 +0100

    rozsireni datoveho modelu

--- django/tournament/models.py
+++ django/tournament/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
-# Create your models here.
-
+#
 class Tournament(models.Model):
     name = models.CharField(max_length = 200)
     slug = models.SlugField()
@@ -16,11 +15,36 @@ class Group(models.Model):
     division = models.ForeignKey(Division, on_delete=models.CASCADE)
     finished = models.BooleanField(default=False)
 
+# teams
 class Team(models.Model):
     name = models.CharField(max_length = 200)
+#    division = models.ForeignKey(Division, on_delete=models.CASCADE)
+
+class TeamPlaceholder(models.Model):
+    name = models.CharField(max_length = 200)
+#    division = models.ForeignKey(Division, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, null = True, on_delete=models.PROTECT)
+
+# seed
+class DivisionSeed(models.Model):
+    rank = models.PositiveSmallIntegerField()
     division = models.ForeignKey(Division, on_delete=models.CASCADE)
+    teamPlaceholder = models.ForeignKey(TeamPlaceholder, on_delete=models.CASCADE)
+
+class DivisionRank(DivisionSeed):
+    pass
+
+class GroupSeed(models.Model):
+    rank = models.PositiveSmallIntegerField()
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    teamPlaceholder = models.ForeignKey(TeamPlaceholder, on_delete=models.CASCADE)
+
+class GroupRank(GroupSeed):
+    pass
+
+    # seed ?
 
 class Match(models.Model):
-    home = models.ForeignKey(Team, related_name = 'home_matches', on_delete=models.CASCADE)
-    away = models.ForeignKey(Team, related_name = 'away_matches', on_delete=models.CASCADE)
-    referee = models.ForeignKey(Team, related_name = 'referee_matches', on_delete=models.CASCADE)
+    home = models.ForeignKey(TeamPlaceholder, related_name = 'home_matches', on_delete=models.CASCADE)
+    away = models.ForeignKey(TeamPlaceholder, related_name = 'away_matches', on_delete=models.CASCADE)
+    referee = models.ForeignKey(TeamPlaceholder, related_name = 'referee_matches', on_delete=models.CASCADE)
