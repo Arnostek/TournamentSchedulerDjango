68952f0e8ad0082bb44acf0c55b4f3ba058d2d1e
Arnost <arnost.chaloupka@zeppelin.com>
2019-03-28 20:43:25 +0100

    prejemnovana semifinale

--- django/tournament/TestData.py
+++ django/tournament/TestData.py
@@ -25,14 +25,14 @@ tdata.AddDivision('Men A','MenA',12)
 # zakladani skupiny
 tdata.actual_division.CreateGroups(['A','B'], tdata.actual_division.seed_placeholders, 1)
 # prvni 4 jdou do semi
-tdata.actual_division.CreateGroups(['S1','S2'], tdata.actual_division.GetGroupsRanks(['A','B'])[:4], 2)
+tdata.actual_division.CreateGroups(['SemiA','SemiB'], tdata.actual_division.GetGroupsRanks(['A','B'])[:4], 2)
 # zapasy o mista bez semi
 for misto in [11,9,7,5]:
     tdata.actual_division.CreateGroups(['{}th'.format(misto)], tdata.actual_division.GetGroupsRanks(['A','B'])[misto - 1: misto + 1], 3)
 # 3rd
-tdata.actual_division.CreateGroups(['3rd'], tdata.actual_division.GetGroupsRanks(['S1','S2'])[2:4], 4)
+tdata.actual_division.CreateGroups(['3rd'], tdata.actual_division.GetGroupsRanks(['SemiA','SemiB'])[2:4], 4)
 # final
-tdata.actual_division.CreateGroups(['final'], tdata.actual_division.GetGroupsRanks(['S1','S2'])[0:2], 5)
+tdata.actual_division.CreateGroups(['final'], tdata.actual_division.GetGroupsRanks(['SemiA','SemiB'])[0:2], 5)
 # vygenerovani zapasu
 tdata.actual_division.CreateMatches()
 
@@ -47,14 +47,14 @@ tdata.actual_division.CreateMatches()
 # Ladies
 tdata.AddDivision('Ladies','Ladies',10)
 tdata.actual_division.CreateGroups(['A','B'], tdata.actual_division.seed_placeholders, 1)
-tdata.actual_division.CreateGroups(['S1','S2'], tdata.actual_division.GetGroupsRanks(['A','B'])[:4], 2)
+tdata.actual_division.CreateGroups(['SemiA','SemiB'], tdata.actual_division.GetGroupsRanks(['A','B'])[:4], 2)
 # zapasy o mista bez semi
 for misto in [9,7,5]:
     tdata.actual_division.CreateGroups(['{}th'.format(misto)], tdata.actual_division.GetGroupsRanks(['A','B'])[misto - 1: misto + 1], 3)
 # 3rd
-tdata.actual_division.CreateGroups(['3rd'], tdata.actual_division.GetGroupsRanks(['S1','S2'])[2:4], 4)
+tdata.actual_division.CreateGroups(['3rd'], tdata.actual_division.GetGroupsRanks(['SemiA','SemiB'])[2:4], 4)
 # final
-tdata.actual_division.CreateGroups(['final'], tdata.actual_division.GetGroupsRanks(['S1','S2'])[0:2], 5)
+tdata.actual_division.CreateGroups(['final'], tdata.actual_division.GetGroupsRanks(['SemiA','SemiB'])[0:2], 5)
 # vygenerovani zapasu
 tdata.actual_division.CreateMatches()
 ####################################################
