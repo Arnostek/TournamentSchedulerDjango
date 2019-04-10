62a6d62ae06d0e5d5c441bd81c0d78b737d563c7
Arnost <arnost.chaloupka@zeppelin.com>
2019-04-10 21:17:09 +0200

    komentare v url

--- django/tournament/urls.py
+++ django/tournament/urls.py
@@ -4,12 +4,16 @@ from . import views
 urlpatterns = [
     path('', views.TournamentListView.as_view()),
     path('tournament-<int:tid>/', views.TournamentDetailView.as_view()),
+    # division system
     path('tournament-<int:tid>/division-<int:did>/system', views.DivisionSystemView.as_view()),
+    # schedule
     path('tournament-<int:tid>/schedule/', views.ScheduleView.as_view()),
     path('tournament-<int:tid>/schedule/division-<int:did>', views.ScheduleView.as_view()),
     path('tournament-<int:tid>/schedule/division-<int:did>-group-<int:gid>', views.ScheduleView.as_view()),
     path('tournament-<int:tid>/schedule/pitch-<int:pid>', views.ScheduleView.as_view()),
     path('tournament-<int:tid>/schedule/team-<int:team>', views.ScheduleView.as_view()),
+
+    # score
     path('set/match-<int:mid>/<slug:who>/<int:score>', views.SetScore),
     #path('<int:id>', views.TournamentDetailView.as_view()),
 ]
