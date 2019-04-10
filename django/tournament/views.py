2bb4c241e941253896f453d604695adedf10dbcb
Arnost <arnost.chaloupka@zeppelin.com>
2019-04-10 21:17:58 +0200

    kosmetika - carka na konci

--- django/tournament/views.py
+++ django/tournament/views.py
@@ -57,7 +57,7 @@ class DivisionSystemView(TemplateView):
         context = {
             'division' : division,
             'groups' : groups,
-            'matches' : division.match_set.all().order_by('group__phase','phase_block','id')
+            'matches' : division.match_set.all().order_by('group__phase','phase_block','id'),
         }
 
         return context
