6fae513dd5685708240b3b6fdd60a98db68f33d4
Arnost <arnost.chaloupka@zeppelin.com>
2019-04-24 09:55:32 +0200

    allowed host je take v .env

--- django/project/settings.py
+++ django/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ['DJANGO_ALLOWED_HOSTS']
 
 
 # Application definition
