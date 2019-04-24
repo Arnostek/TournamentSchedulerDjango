b0fe0755fb9956f0032c07d780b831def1676e9b
Arnost <arnost.chaloupka@zeppelin.com>
2019-04-24 09:46:00 +0200

    Djnago sercret key je v env

--- django/project/settings.py
+++ django/project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!otqn506+4k2ffly+&pp*%9i**ljk#7m7_lf=34d*r*r0mybj8'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
