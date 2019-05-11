2619a38cc657485bf6e09864adccb914f9154341
Arnost <arnost.chaloupka@zeppelin.com>
2019-05-11 23:58:00 +0200

    MenA vygenerovany tridou TwoGroups

--- django/tournament/TestData.py
+++ django/tournament/TestData.py
@@ -25,8 +25,9 @@ class TestData:
 tdata = TestData("Prague 2019", "PRG2019")
 ####################################################
 # men A
+MenA_system = TwoGroups(tdata.t,'Men A','MenA',12)
 tdata.AddDivision('Men A','MenA',12)
-tdata.actual_division.CreateTeams(
+MenA_system.division.CreateTeams(
     [
         "Gottingen",
         "KV Nürnberg",
@@ -42,19 +43,6 @@ tdata.actual_division.CreateTeams(
         "9ValA",
     ]
 )
-# zakladani skupiny
-tdata.actual_division.CreateGroups(['A','B'], tdata.actual_division.seed_placeholders, 1)
-# prvni 4 jdou do semi
-tdata.actual_division.CreateGroups(['SemiA','SemiB'], tdata.actual_division.GetGroupsRanks(['A','B'])[:4], 2)
-# zapasy o mista bez semi
-for misto in [11,9,7,5]:
-    tdata.actual_division.CreateGroups(['{}th'.format(misto)], tdata.actual_division.GetGroupsRanks(['A','B'])[misto - 1: misto + 1], 3)
-# 3rd
-tdata.actual_division.CreateGroups(['3rd'], tdata.actual_division.GetGroupsRanks(['SemiA','SemiB'])[2:4], 4)
-# final
-tdata.actual_division.CreateGroups(['final'], tdata.actual_division.GetGroupsRanks(['SemiA','SemiB'])[0:2], 5)
-# vygenerovani zapasu
-tdata.actual_division.CreateMatches()
 
 ####################################################
 # men B
