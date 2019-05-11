2bc72f0f8342e9cb9322dac4aefee10f51c27b41
Arnost <arnost.chaloupka@zeppelin.com>
2019-05-11 22:44:06 +0200

    System s jednou zakladni skupinou, kazdy s kazdym

---
+++ django/tournament/systems/SingleGroupDivisionSystem.py
@@ -0,0 +1,24 @@
+from .DivisionSystemBase import DivisionSystemBase
+
+class SingleGroupDivisionSystem(DivisionSystemBase):
+    """ Jedna zakladni skupina, kazdy s kazdym, zapas o 3. a prvni misto """
+
+    def __init__(self,tournament,division_name,division_slug,num_of_teams):
+        # zavolam konsturktor Predka
+        super(SingleGroupDivisionSystem, self).__init__(tournament,division_name,division_slug,num_of_teams)
+        # vytvorim system
+        self._createSystem()
+        # vygeneruji zapasy
+        self._createMatches()
+
+    def _createSystem(self):
+        # phase 1 - zakladni skupina
+        phase = 1
+        self.division.CreateGroups(['A'], self.division.seed_placeholders, phase)
+        a_ranks = self.division.GetGroupsRanks(['A'])
+        # 3rd
+        phase += 1
+        self.division.CreateGroups(['3rd'], [a_ranks[2], a_ranks[3]] , phase)
+        # final
+        phase += 1
+        self.division.CreateGroups(['final'],[a_ranks[0], a_ranks[1]] , phase)
