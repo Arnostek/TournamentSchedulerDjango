8de7ed0ae74453c9ddebb1a4fc79878ccbf7c921
Arnost <arnost.chaloupka@zeppelin.com>
2019-06-07 13:30:20 +0200

    typo in path

--- django/project/settings.py
+++ django/project/settings.py
@@ -122,5 +122,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "tounament/static"),
+    os.path.join(BASE_DIR, "tournament/static"),
 ]
