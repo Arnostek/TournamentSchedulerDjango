9c44f38d42e758342f8837f62a206711906af8af
Arnost <arnost.chaloupka@zeppelin.com>
2019-06-29 20:42:11 +0200

    moved lines

--- django/tournament/hotfixes/find_conflicts.py
+++ django/tournament/hotfixes/find_conflicts.py
@@ -1,18 +1,14 @@
 import pandas as pd
-
-# nacteni schedule do df
-[{ 'time' : sch.time, 'pitch' : sch.pitch.name, 'match': sch.match }for sch in Tournament.objects.last().schedule_set.all()]
-df = pd.DataFrame([{ 'time' : sch.time, 'pitch' : sch.pitch.name, 'match': sch.match }for sch in Tournament.objects.last().schedule_set.all()])
-df = df.pivot(columns='pitch',index='time',values='match')
-df
-
-
 # kontrola dvou zapasu
 def has_conflict(m1,m2):
     for tph1 in [m1.home,m1.away,m1.referee]:
         for tph2 in [m2.home,m2.away,m2.referee]:
             if tph1 == tph2:
                 print ("Problem match num {} team {}".format(m1,tph1.team.name))
+
+# create schedule dataframe
+df = pd.DataFrame([{ 'time' : sch.time, 'pitch' : sch.pitch.name, 'match': sch.match }for sch in Tournament.objects.last().schedule_set.all()])
+df = df.pivot(columns='pitch',index='time',values='match')
 
 # check all
 for i in range(len(df)-1):
