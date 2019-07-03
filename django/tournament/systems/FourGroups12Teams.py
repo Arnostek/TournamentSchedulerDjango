618309fcf0e344e09f893903c4d7f49b6f00bdf4
Arnost <arnost.chaloupka@zeppelin.com>
2019-07-03 11:26:38 +0200

    fixed bug in seeding of groups X a Y

--- django/tournament/systems/FourGroups12Teams.py
+++ django/tournament/systems/FourGroups12Teams.py
@@ -25,7 +25,7 @@ class FourGroups12Teams(DivisionSystemBase):
         # phase 2 - two groups up, one down
         phase += 1
         phase1_ranks = self.division.GetGroupsRanks(['A','B','C','D'])
-        self.division.CreateGroups(['X','Y'], phase1_ranks[:8], phase, referee_groups = ['Y','X'])
+        self.division.CreateGroups(['X','Y'], [phase1_ranks[0],phase1_ranks[4],phase1_ranks[1],phase1_ranks[5],phase1_ranks[2],phase1_ranks[6],phase1_ranks[3],phase1_ranks[7]], phase, referee_groups = ['Y','X'])
         self.division.CreateGroups(['Z'],phase1_ranks[8:], phase, referee_groups = ['Z'])
         # first 4 teams go to semi
         phase += 1
