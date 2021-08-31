85ea16deb48eca1031def918b6aff832515aac0d
Arnost <arnost@zcz-wn-arnost.zeppelin.corp>
2021-08-31 19:58:56 +0200

    conditional semi

--- django/tournament/systems/SingleGroupDivisionSystem.py
+++ django/tournament/systems/SingleGroupDivisionSystem.py
@@ -3,9 +3,11 @@ from .DivisionSystemBase import DivisionSystemBase
 class SingleGroupDivisionSystem(DivisionSystemBase):
     """ Jedna zakladni skupina, kazdy s kazdym, zapas o 3. a prvni misto """
 
-    def __init__(self,tournament,division_name,division_slug,num_of_teams):
+    def __init__(self,tournament,division_name,division_slug,num_of_teams,semi=False):
         # zavolam konsturktor Predka
         super(SingleGroupDivisionSystem, self).__init__(tournament,division_name,division_slug,num_of_teams)
+        # bude semi?
+        self.semi = semi
         # vytvorim system
         self._createSystem()
         # vygeneruji zapasy
@@ -18,17 +20,34 @@ class SingleGroupDivisionSystem(DivisionSystemBase):
         phase = 1
         self.division.CreateGroups(['A'], self.division.seed_placeholders, phase, ['A'])
         a_ranks = self.division.GetGroupsRanks(['A'])
-        # 3rd
-        if len(a_ranks) > 3:
         # 5th
         if len(a_ranks) > 5:
             phase += 1
             self.division.CreateGroups(['5th'], [a_ranks[4], a_ranks[5]] , phase)
+
+        if self.semi:
+            # semi
             phase += 1
-            self.division.CreateGroups(['3rd'], [a_ranks[2], a_ranks[3]] , phase)
-        # final
-        phase += 1
-        self.division.CreateGroups(['final'],[a_ranks[0], a_ranks[1]] , phase)
+            self.division.CreateGroups(['Semi1'], [a_ranks[1], a_ranks[2]] , phase)
+            # final
+            phase += 1
+            self.division.CreateGroups(['Semi2'],[a_ranks[0], a_ranks[3]] , phase)
+
+            semi_ranks = self.division.GetGroupsRanks(['Semi1','Semi2'])
+            # 3rd
+            phase += 1
+            self.division.CreateGroups(['3rd'], [semi_ranks[2], semi_ranks[3]] , phase)
+            # final
+            phase += 1
+            self.division.CreateGroups(['final'],[semi_ranks[0], semi_ranks[1]] , phase)
+        else:
+            # 3rd
+            if len(a_ranks) > 3:
+                phase += 1
+                self.division.CreateGroups(['3rd'], [a_ranks[2], a_ranks[3]] , phase)
+            # final
+            phase += 1
+            self.division.CreateGroups(['final'],[a_ranks[0], a_ranks[1]] , phase)
 
     def _addReferees(self):
         """ Doplneni rozhodcich pro finalove zapasy """
