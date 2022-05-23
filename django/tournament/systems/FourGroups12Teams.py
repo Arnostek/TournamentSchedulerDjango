a1804a9d852441f43d634a3d8f2cf8d5546e1dc8
Arnost <arnost.chaloupka@zeppelin.com>
2022-05-23 22:21:09 +0200

    skupiny prejmenovane na E,F a G

--- django/tournament/systems/FourGroups12Teams.py
+++ django/tournament/systems/FourGroups12Teams.py
@@ -25,20 +25,20 @@ class FourGroups12Teams(DivisionSystemBase):
         # phase 2 - two groups up, one down
         phase += 1
         phase1_ranks = self.division.GetGroupsRanks(['A','B','C','D'])
-        self.division.CreateGroups(['X','Y'], [phase1_ranks[0],phase1_ranks[4],phase1_ranks[1],phase1_ranks[5],phase1_ranks[2],phase1_ranks[6],phase1_ranks[3],phase1_ranks[7]], phase, referee_groups = ['Y','X'])
-        self.division.CreateGroups(['Z'],phase1_ranks[8:], phase, referee_groups = ['Z'])
+        self.division.CreateGroups(['E','F'], [phase1_ranks[0],phase1_ranks[4],phase1_ranks[1],phase1_ranks[5],phase1_ranks[2],phase1_ranks[6],phase1_ranks[3],phase1_ranks[7]], phase, referee_groups = ['F','E'])
+        self.division.CreateGroups(['G'],phase1_ranks[8:], phase, referee_groups = ['G'])
         # first 4 teams go to semi
         phase += 1
-        z_ranks = self.division.GetGroupsRanks(['Z'])
-        xy_ranks = self.division.GetGroupsRanks(['X','Y'])
-        self.division.CreateGroups(['SemiA','SemiB'], xy_ranks[:4], phase)
+        g_ranks = self.division.GetGroupsRanks(['G'])
+        ef_ranks = self.division.GetGroupsRanks(['E','F'])
+        self.division.CreateGroups(['SemiA','SemiB'], ef_ranks[:4], phase)
         # Finals
         phase += 1
 
-        self.division.CreateGroups(['11th'],z_ranks[2:] , phase)
-        self.division.CreateGroups(['9th'],z_ranks[:2] , phase)
-        self.division.CreateGroups(['7th'],xy_ranks[6:8] , phase)
-        self.division.CreateGroups(['5th'],xy_ranks[4:6] , phase)
+        self.division.CreateGroups(['11th'],g_ranks[2:] , phase)
+        self.division.CreateGroups(['9th'],g_ranks[:2] , phase)
+        self.division.CreateGroups(['7th'],ef_ranks[6:8] , phase)
+        self.division.CreateGroups(['5th'],ef_ranks[4:6] , phase)
         # 3rd
         phase += 1
         self.division.CreateGroups(['3rd'], self.division.GetGroupsRanks(['SemiA','SemiB'])[2:4], phase)
@@ -48,13 +48,13 @@ class FourGroups12Teams(DivisionSystemBase):
 
     def _addReferees(self):
         """ Referees for final matches """
-        z_ranks = self.division.GetGroupsRanks(['Z'])
-        xy_ranks = self.division.GetGroupsRanks(['X','Y'])
-        self._GroupAddReferees('SemiA',[xy_ranks[4]])
-        self._GroupAddReferees('SemiB',[xy_ranks[5]])
-        self._GroupAddReferees('11th',[xy_ranks[7]])
-        self._GroupAddReferees('9th',[xy_ranks[5]])
-        self._GroupAddReferees('7th',[z_ranks[2]])
-        self._GroupAddReferees('5th',[z_ranks[0]])
-        self._GroupAddReferees('3rd',[xy_ranks[6]])
-        self._GroupAddReferees('final',[xy_ranks[4]])
+        g_ranks = self.division.GetGroupsRanks(['G'])
+        ef_ranks = self.division.GetGroupsRanks(['E','F'])
+        self._GroupAddReferees('SemiA',[ef_ranks[4]])
+        self._GroupAddReferees('SemiB',[ef_ranks[5]])
+        self._GroupAddReferees('11th',[ef_ranks[7]])
+        self._GroupAddReferees('9th',[ef_ranks[5]])
+        self._GroupAddReferees('7th',[g_ranks[2]])
+        self._GroupAddReferees('5th',[g_ranks[0]])
+        self._GroupAddReferees('3rd',[ef_ranks[6]])
+        self._GroupAddReferees('final',[ef_ranks[4]])
