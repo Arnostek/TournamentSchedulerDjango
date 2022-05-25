5785dc312b8fde594a28765d24b2295fa9e67d7c
Arnost <arnost.chaloupka@zeppelin.com>
2022-05-25 16:03:56 +0200

    m1 muzeme inicializovat uz vyse

--- django/tournament/views.py
+++ django/tournament/views.py
@@ -271,8 +271,8 @@ def FindConflicts(request, tid):
     df = df.pivot(columns='pitch',index='time',values='match')
     for i in range(len(df)-1):
         for p1 in range(len(df.columns)):
+            m1 = df.iloc[i,p1]
             for p2 in range(len(df.columns)):
-                m1 = df.iloc[i,p1]
                 for m2 in [df.iloc[i,p2],df.iloc[i+1,p2]]:
                     if m1 and m2 and m1 != m2:
                         for tph1 in [m1.home,m1.away,m1.referee]:
