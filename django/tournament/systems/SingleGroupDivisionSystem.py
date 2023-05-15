b217a0ad8dfac8184c8a87a5c6954d214382a1de
Arnost <arnost.chaloupka@zeppelin.com>
2023-05-15 13:04:58 +0200

    mista az po semi

--- django/tournament/systems/SingleGroupDivisionSystem.py
+++ django/tournament/systems/SingleGroupDivisionSystem.py
@@ -22,16 +22,20 @@ class SingleGroupDivisionSystem(DivisionSystemBase):
         phase = 1
         self.division.CreateGroups(['A'], self.division.seed_placeholders, phase, ['A'])
         a_ranks = self.division.GetGroupsRanks(['A'])
+
+        # semi
+        if self.semi:
+            phase += 1
+            self.division.CreateGroups(['Semi1'], [a_ranks[1], a_ranks[2]] , phase)
+            self.division.CreateGroups(['Semi2'],[a_ranks[0], a_ranks[3]] , phase)
+
         # 5th
         if len(a_ranks) > 5 and self.final_for >= 5:
             phase += 1
             self.division.CreateGroups(['5th'], [a_ranks[4], a_ranks[5]] , phase)
 
+        # final
         if self.semi:
-            # semi
-            phase += 1
-            self.division.CreateGroups(['Semi1'], [a_ranks[1], a_ranks[2]] , phase)
-            self.division.CreateGroups(['Semi2'],[a_ranks[0], a_ranks[3]] , phase)
 
             semi_ranks = self.division.GetGroupsRanks(['Semi1','Semi2'])
             # 3rd
@@ -40,6 +44,7 @@ class SingleGroupDivisionSystem(DivisionSystemBase):
             # final
             phase += 1
             self.division.CreateGroups(['final'],[semi_ranks[0], semi_ranks[1]] , phase)
+
         else:
             # 3rd
             if len(a_ranks) > 3 and self.final_for >= 3:
