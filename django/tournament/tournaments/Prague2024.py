ee2d2bcce241c19add58650f607e9f943bb9dbe7
Arnost <arnost.chaloupka@zeppelin.com>
2024-05-16 17:07:52 +0200

    zmeny v teamech

--- django/tournament/tournaments/Prague2024.py
+++ django/tournament/tournaments/Prague2024.py
@@ -8,6 +8,7 @@ from tournament.systems.TwoGroups import TwoGroups
 # from tournament.systems.TwoGroups8TeamsMiddle import TwoGroups8TeamsMiddle
 # from tournament.systems.FourGroups12Teams import FourGroups12Teams
 from tournament.systems.FourGroups16Teams import FourGroups16Teams
+from tournament.systems.FourGroups15Teams import FourGroups15Teams
 
 from tournament.TournamentScheduler import TournamentScheduler
 
@@ -16,7 +17,7 @@ import pytz
 # run in shell:
 # docker-compose exec tournament_scheduler python /srv/django/manage.py shell -c 'from tournament.tournaments import Prague2024'
 
-# models.Tournament.objects.get(slug='PIT2024_TEST001').delete()
+models.Tournament.objects.get(slug='PIT2024_TEST001').delete()
 
 # turnaj
 prague2024 = models.Tournament(name = "PIT 2024 TEST", slug = "PIT2024_TEST001")
@@ -25,7 +26,7 @@ print(prague2024)
 ####################################################
 # men 1
 
-Men1_system = FourGroups16Teams(prague2024,'Men Elite','MenElite',16)
+Men1_system = FourGroups15Teams(prague2024,'Men Elite','MenElite',15)
 Men1_system.division.CreateTeams(
     [
         "RKV Berlin",
@@ -39,7 +40,7 @@ Men1_system.division.CreateTeams(
         "Poland U21",
         "UKK Wien",
         "Bremen",
-        "Austria U21",
+        # "Austria U21",
         "Kwisa Lesna",
         "Blue Men (Hungary)",
         "Nagymaros",
@@ -47,9 +48,26 @@ Men1_system.division.CreateTeams(
     ]
 )
 
+
+####################################################
+# Ladies
+Ladies_system = TwoGroups8Teams(prague2024,'Ladies','Ladies',8)
+Ladies_system.division.CreateTeams(
+    [
+        "UKS SET Kaniów Women",
+        "KSVH Women",
+        "UKS Kanu Katowice",
+        "VMW Berlin Women",
+        "SG Sachsen",
+        "SG Dresden",
+        "Czech U21 Women",
+        "Czech/German Women",
+    ]
+)
+
+
 ####################################################
 # U15
-
 U15_system = TwoGroups(prague2024,'U15','U15',11)
 U15_system.division.CreateTeams(
     [
@@ -65,22 +83,6 @@ U15_system.division.CreateTeams(
         "Nagymaros U15",
         "VK Berlin U15",
 
-    ]
-)
-
-####################################################
-# Ladies
-Ladies_system = TwoGroups8Teams(prague2024,'Ladies','Ladies',8)
-Ladies_system.division.CreateTeams(
-    [
-        "UKS SET Kaniów Women",
-        "KSVH Women",
-        "UKS Kanu Katowice",
-        "VMW Berlin Women",
-        "SG Sachsen",
-        "SG Dresden",
-        "Czech U21 Women",
-        "Czech/German Women",
     ]
 )
 
