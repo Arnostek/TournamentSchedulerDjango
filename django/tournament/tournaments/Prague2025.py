89cfce8d89bfaa01978c4a65c4b6c6ece6e2cc14
Arnost <chaloua@volny.cz>
2025-05-06 20:50:43 +0200

    slug as variable

--- django/tournament/tournaments/Prague2025.py
+++ django/tournament/tournaments/Prague2025.py
@@ -20,7 +20,8 @@ import pytz
 # docker compose exec tournament_scheduler python /srv/django/manage.py shell -c 'from tournament.tournaments import Prague2025'
 
 # turnaj
-prague2025 = models.Tournament(name = "PIT 2025 TEST", slug = "PIT2025_TEST07")
+tslug = "PIT2025_TEST09"
+prague2025 = models.Tournament(name = tslug, slug = tslug)
 prague2025.save()
 print(prague2025)
 ####################################################
