cb4016940db59a442cf2a90f677faab7589caf1c
Arnost <chaloua@volny.cz>
2025-05-11 14:57:41 +0200

    choczno neprijede

--- django/tournament/tournaments/Prague2025.py
+++ django/tournament/tournaments/Prague2025.py
@@ -20,7 +20,7 @@ import pytz
 # docker compose exec tournament_scheduler python /srv/django/manage.py shell -c 'from tournament.tournaments import Prague2025'
 
 # turnaj
-tslug = "PIT2025_TEST30"
+tslug = "PIT2025_TEST31"
 prague2025 = models.Tournament(name = tslug, slug = tslug)
 prague2025.save()
 print(prague2025)
@@ -51,12 +51,12 @@ Men1_system.division.CreateTeams(
 
 ####################################################
 # Ladies
-Ladies_system = TwoGroups(prague2025,'Ladies','Ladies',10, semi5_8=True)
+Ladies_system = TwoGroups(prague2025,'Ladies','Ladies',9, semi5_8=True)
 Ladies_system.division.CreateTeams(
      [
         "VMW Berlin Women", 
         "Kaniow Women", 
-        "MOSW Choszczno Women", 
+        #"MOSW Choszczno Women", 
         "Katowice Women", 
         "Prague Women", 
         "Swiss U21", 
