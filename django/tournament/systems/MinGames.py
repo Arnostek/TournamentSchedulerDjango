44b05ce9a14b7caaaeaee238bb48831e2074cdf6
Arnost <chaloua@volny.cz>
2026-04-08 13:51:35 +0200

    min games refactoring

--- django/tournament/systems/MinGames.py
+++ django/tournament/systems/MinGames.py
@@ -16,13 +16,18 @@ class MinGames16Teams(DivisionSystemBase):
 
     def _createSystem(self):
         # phase 1 - zakladni skupiny
-        phase = 1
-        self.division.CreateGroups(['A','B','C','D'], self.division.seed_placeholders, phase, ['C','D','A','B'])
+        self.phase.create_groups(
+            ['A','B','C','D'],
+            self.division.seed_placeholders,
+            referee_groups=['C','D','A','B']
+        )
+        phase1_ranks = self.phase.get_ranks(['A','B','C','D'])
+
         # dolni a horni skupina, kde kazdy hraje jednou, zapasy vygenerujeme rucne
-        phase += 1
-        self.division.CreateGroups(['E'], self.division.GetGroupsRanks(['A','B','C','D'])[:8], phase)
+        self.phase.next_phase()
+        self.phase.create_groups(['E'], phase1_ranks[:8])
         e_group = self.division.group_set.last()
-        self.division.CreateGroups(['F'], self.division.GetGroupsRanks(['A','B','C','D'])[8:], phase)
+        self.phase.create_groups(['F'], phase1_ranks[8:])
         f_group = self.division.group_set.last()
 
         # do E a F prenasime body z minula
@@ -46,41 +51,59 @@ class MinGames16Teams(DivisionSystemBase):
         e_group.CreateMatch((4,5,3))
         f_group.CreateMatch((4,5,3))
         # semi - prvni 4 tymy z horni skupiny
-        phase += 1
-        self.division.CreateGroups(['SemiA','SemiB'], self.division.GetGroupsRanks(['E'])[:4], phase)
+        self.phase.next_phase()
+        self.phase.create_groups(['SemiA','SemiB'], self.phase.get_ranks(['E'])[:4])
+
         # zapasy o mista bez semi
-        phase += 1
+        self.phase.next_phase()
+        f_ranks = self.phase.get_ranks(['F'])
+        e_ranks = self.phase.get_ranks(['E'])
+
+        # Create placement matches with ranks
         for misto in [15,13,11,9]:
-            self.division.CreateGroups(['{}th'.format(misto)], self.division.GetGroupsRanks(['F'])[misto - 9: misto -7], phase)
+            self.division.CreateGroups(['{}th'.format(misto)], f_ranks[misto - 9: misto -7], self.phase.phase)
+            self.division.CreateRanks(misto, self.division.GetGroupsRanks(['{}th'.format(misto)]))
+
         for misto in [7,5]:
-            self.division.CreateGroups(['{}th'.format(misto)], self.division.GetGroupsRanks(['E'])[misto - 1: misto + 1], phase)
+            self.division.CreateGroups(['{}th'.format(misto)], e_ranks[misto - 1: misto + 1], self.phase.phase)
+            self.division.CreateRanks(misto, self.division.GetGroupsRanks(['{}th'.format(misto)]))
+
         # 3rd
-        phase += 1
-        self.division.CreateGroups(['3rd'], self.division.GetGroupsRanks(['SemiA','SemiB'])[2:4], phase)
+        self.phase.next_phase()
+        self.phase.create_and_rank('3rd', self.phase.get_ranks(['SemiA','SemiB'])[2:4], 3)
+
         # final
-        phase += 1
-        self.division.CreateGroups(['final'], self.division.GetGroupsRanks(['SemiA','SemiB'])[0:2], phase)
+        self.phase.next_phase()
+        self.phase.create_and_rank('final', self.phase.get_ranks(['SemiA','SemiB'])[0:2], 1)
 
     def _addReferees(self):
         """ Doplneni rozhodcich pro finalove zapasy """
         # gropy E a F
-        phase1_ranks = self.division.GetGroupsRanks(['A','B','C','D'])
-        self._GroupAddReferees('E',[phase1_ranks[0],phase1_ranks[1],phase1_ranks[2],phase1_ranks[3]])
-        self._GroupAddReferees('F',[phase1_ranks[8],phase1_ranks[9],phase1_ranks[10],phase1_ranks[11]])
+        phase1_ranks = self.phase.get_ranks(['A','B','C','D'])
+        self.referees.assign_multiple_referees({
+            'E': [phase1_ranks[0],phase1_ranks[1],phase1_ranks[2],phase1_ranks[3]],
+            'F': [phase1_ranks[8],phase1_ranks[9],phase1_ranks[10],phase1_ranks[11]],
+        })
+
         # Semi
-        groupE_ranks = self.division.GetGroupsRanks(['E'])
-        groupF_ranks = self.division.GetGroupsRanks(['F'])
-        self._GroupAddReferees('SemiA',[groupE_ranks[4]])
-        self._GroupAddReferees('SemiB',[groupE_ranks[5]])
+        groupE_ranks = self.phase.get_ranks(['E'])
+        groupF_ranks = self.phase.get_ranks(['F'])
+        self.referees.assign_multiple_referees({
+            'SemiA': [groupE_ranks[4]],
+            'SemiB': [groupE_ranks[5]],
+        })
+
         # umisteni
-        self._GroupAddReferees('15th',[groupF_ranks[0]])
-        self._GroupAddReferees('13th',[groupF_ranks[1]])
-        self._GroupAddReferees('11th',[groupF_ranks[7]])
-        self._GroupAddReferees('9th',[groupF_ranks[6]])
-        self._GroupAddReferees('7th',[groupF_ranks[4]])
-        self._GroupAddReferees('5th',[self.division.GetGroupsRanks(['11th'])[0]])
-        self._GroupAddReferees('3rd',[self.division.GetGroupsRanks(['9th'])[0]])
-        self._GroupAddReferees('final',[self.division.GetGroupsRanks(['7th'])[0]])
+        self.referees.assign_multiple_referees({
+            '15th': [groupF_ranks[0]],
+            '13th': [groupF_ranks[1]],
+            '11th': [groupF_ranks[7]],
+            '9th': [groupF_ranks[6]],
+            '7th': [groupF_ranks[4]],
+            '5th': [self.division.GetGroupsRanks(['11th'])[0]],
+            '3rd': [self.division.GetGroupsRanks(['9th'])[0]],
+            'final': [self.division.GetGroupsRanks(['7th'])[0]],
+        })
 
 
 class MinGames6Teams(DivisionSystemBase):
@@ -95,11 +118,10 @@ class MinGames6Teams(DivisionSystemBase):
         self._createMatches()
 
     def _createSystem(self):
-        phase = 1
-        self.division.CreateGroups(['A','B'], self.division.seed_placeholders, phase)
+        self.phase.create_groups(['A','B'], self.division.seed_placeholders)
         # skupina, kde se neodehraje vsechno, zapasy vygenerujeme rucne
-        phase += 1
-        self.division.CreateGroups(['C'], self.division.GetGroupsRanks(['A','B']), phase)
+        self.phase.next_phase()
+        self.phase.create_groups(['C'], self.phase.get_ranks(['A','B']))
         c_group = self.division.group_set.last()
         c_group.CreateMatch((1,4,0))
         c_group.CreateMatch((3,6,0))
@@ -107,14 +129,18 @@ class MinGames6Teams(DivisionSystemBase):
         c_group.CreateMatch((1,6,1))
         c_group.CreateMatch((3,2,1))
         c_group.CreateMatch((5,4,1))
+
         #5th
-        phase += 1
-        self.division.CreateGroups(['5th'], self.division.GetGroupsRanks(['C'])[4:6], phase)
+        self.phase.next_phase()
+        self.phase.create_and_rank('5th', self.phase.get_ranks(['C'])[4:6], 5)
+
         # 3rd
-        phase += 1
-        self.division.CreateGroups(['3rd'], self.division.GetGroupsRanks(['C'])[2:4], phase)
+        self.phase.next_phase()
+        self.phase.create_and_rank('3rd', self.phase.get_ranks(['C'])[2:4], 3)
+
         # final
-        phase += 1
-        self.division.CreateGroups(['final'], self.division.GetGroupsRanks(['C'])[0:2], phase)
+        self.phase.next_phase()
+        self.phase.create_and_rank('final', self.phase.get_ranks(['C'])[0:2], 1)
+
         # vygenerovani zapasu
         self.division.CreateMatches()
