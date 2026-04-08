b5c4ce47233e6acfc8cf6497010a7e5b020adf3e
Arnost <chaloua@volny.cz>
2026-04-08 13:53:00 +0200

    refactoring

--- django/tournament/systems/ThreeGroups15Teams.py
+++ django/tournament/systems/ThreeGroups15Teams.py
@@ -15,60 +15,65 @@ class ThreeGroups15Teams(DivisionSystemBase):
         self._addReferees()
 
     def _createSystem(self):
-        # phase 1 - zakladni skupina. Skupiny 5,5,5
-        phase = 1
-        self.division.CreateGroups(['A','B','C'], self.division.seed_placeholders, phase, ['A','B','C'])
 
-        # meziskupiny
-        phase += 1
-        # 1- 6
-        self.division.CreateGroups(['E'], [self.division.GetGroupsRanks(['A'])[0], self.division.GetGroupsRanks(['B'])[1], self.division.GetGroupsRanks(['C'])[0]], phase, ['E'])
-        self.division.CreateGroups(['F'], [self.division.GetGroupsRanks(['A'])[1], self.division.GetGroupsRanks(['B'])[0], self.division.GetGroupsRanks(['C'])[1]], phase, ['F'])
-        # 7- 12
-        self.division.CreateGroups(['G'], [self.division.GetGroupsRanks(['A'])[2], self.division.GetGroupsRanks(['B'])[3], self.division.GetGroupsRanks(['C'])[2]], phase, ['G'])
-        self.division.CreateGroups(['H'], [self.division.GetGroupsRanks(['A'])[3], self.division.GetGroupsRanks(['B'])[2], self.division.GetGroupsRanks(['C'])[3]], phase, ['H'])
+        # PHASE 1 - Three base groups
+        self.phase.create_groups(
+            ['A', 'B', 'C'],
+            self.division.seed_placeholders,
+            referee_groups=['A', 'B', 'C']
+        )
 
-        # # SF pro 1-4
-        phase += 1
-        self.division.CreateGroups(['SF1','SF2'], self.division.GetGroupsRanks(['E','F'])[:4], phase)
+        # PHASE 2 - Inter-group matches (complex seeding with PlacementBuilder)
+        self.phase.next_phase()
+        a_ranks = self.phase.get_ranks(['A'])
+        b_ranks = self.phase.get_ranks(['B'])
+        c_ranks = self.phase.get_ranks(['C'])
 
-        # Places
-        phase += 1
-        # last3
-        self.division.CreateGroups(['Last3'],self.division.GetGroupsRanks(['A','B','C'])[12:], phase,['Last3'])
-        self.division.CreateRanks(13,self.division.GetGroupsRanks(['Last3']))
+        # Top 6 teams
+        self.phase.create_groups(['E'], [a_ranks[0], b_ranks[1], c_ranks[0]], referee_groups=['E'])
+        self.phase.create_groups(['F'], [a_ranks[1], b_ranks[0], c_ranks[1]], referee_groups=['F'])
 
-        # zapasy o mista
-        phase += 1
-        self.division.CreateGroups(['11th'], self.division.GetGroupsRanks(['G','H'])[4:6], phase)
-        self.division.CreateRanks(11,self.division.GetGroupsRanks(['11th']))
+        # Places 7-12
+        self.phase.create_groups(['G'], [a_ranks[2], b_ranks[3], c_ranks[2]], referee_groups=['G'])
+        self.phase.create_groups(['H'], [a_ranks[3], b_ranks[2], c_ranks[3]], referee_groups=['H'])
 
-        self.division.CreateGroups(['9th'], self.division.GetGroupsRanks(['G','H'])[2:4], phase)
-        self.division.CreateRanks(9,self.division.GetGroupsRanks(['9th']))
+        # PHASE 3 - Semis from top-ranked groups
+        self.phase.next_phase()
+        ef_ranks = self.phase.get_ranks(['E', 'F'])
+        self.phase.create_groups(['SF1', 'SF2'], ef_ranks[:4])
 
-        self.division.CreateGroups(['7th'], self.division.GetGroupsRanks(['G','H'])[:2], phase)
-        self.division.CreateRanks(7,self.division.GetGroupsRanks(['7th']))
+        # PHASE 4 - Placements using builder
+        self.phase.next_phase()
+        gh_ranks = self.phase.get_ranks(['G', 'H'])
 
-        self.division.CreateGroups(['5th'], self.division.GetGroupsRanks(['E','F'])[4:], phase)
-        self.division.CreateRanks(5,self.division.GetGroupsRanks(['5th']))
+        self.placements.create_placements_from_ranges({
+            11: gh_ranks[4:6],
+            9: gh_ranks[2:4],
+            7: gh_ranks[:2],
+            5: ef_ranks[4:],
+        })
 
-        self.division.CreateGroups(['3rd'], self.division.GetGroupsRanks(['SF1','SF2'])[2:], phase)
-        self.division.CreateRanks(3,self.division.GetGroupsRanks(['3rd']))
+        # PHASE 5 - Last3 and Finals
+        self.phase.next_phase()
+        abc_ranks = self.phase.get_ranks(['A', 'B', 'C'])
+        self.last3_builder.create_last3_group(['A', 'B', 'C'])
 
-        # Final
-        phase += 1
-        self.division.CreateGroups(['Final'], self.division.GetGroupsRanks(['SF1','SF2'])[:2], phase)
-        self.division.CreateRanks(1,self.division.GetGroupsRanks(['Final']))
+        sf_ranks = self.phase.get_ranks(['SF1', 'SF2'])
+        self.phase.create_and_rank('3rd', sf_ranks[2:4], 3)
+        self.phase.create_and_rank('Final', sf_ranks[0:2], 1)
 
     def _addReferees(self):
-        """ Referees for  matches """
-        gh_ranks = self.division.GetGroupsRanks(['G','H'])
-        ef_ranks = self.division.GetGroupsRanks(['E','F'])
-        self._GroupAddReferees('SF1',[ef_ranks[4]])
-        self._GroupAddReferees('SF2',[ef_ranks[5]])
-        self._GroupAddReferees('11th',[ef_ranks[3]])
-        self._GroupAddReferees('9th',[ef_ranks[2]])
-        self._GroupAddReferees('7th',[gh_ranks[2]])
-        self._GroupAddReferees('5th',[gh_ranks[0]])
-        self._GroupAddReferees('3rd',[ef_ranks[5]])
-        self._GroupAddReferees('Final',[ef_ranks[4]])
+        """Clean referee assignment using builder."""
+        gh_ranks = self.phase.get_ranks(['G', 'H'])
+        ef_ranks = self.phase.get_ranks(['E', 'F'])
+
+        self.referees.assign_multiple_referees({
+            'SF1': [ef_ranks[4]],
+            'SF2': [ef_ranks[5]],
+            '11th': [ef_ranks[3]],
+            '9th': [ef_ranks[2]],
+            '7th': [gh_ranks[2]],
+            '5th': [gh_ranks[0]],
+            '3rd': [ef_ranks[5]],
+            'Final': [ef_ranks[4]],
+        })
