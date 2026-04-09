968e8dce7c431bee1c272156ab993d20f0443692
Arnost <chaloua@volny.cz>
2026-04-10 01:02:40 +0200

    test ovani

---
+++ django/tournament/scheduler/ortools_test.py
@@ -0,0 +1,14 @@
+from ortools.sat.python import cp_model
+from tournament.scheduler.ortools_build_solver_input import build_solver_input
+from tournament.scheduler.ortools_build_model import build_model
+data = build_solver_input()
+
+slots = list(range(70))      # např. 10 časů
+pitches = list(range(5))    # např. 3 hřiště
+
+model_data = build_model(data, slots, pitches)
+
+solver = cp_model.CpSolver()
+solver.parameters.max_time_in_seconds = 60
+# solver.parameters.log_search_progress = True
+result = solver.Solve(model_data["model"])
