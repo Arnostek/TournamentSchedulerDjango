72d24de497ed273d90a74e1c8da64bc07d6d465c
Arnost <chaloua@volny.cz>
2026-04-15 08:30:02 +0200

    testovaci turnaj

---
+++ django/tournament/tournaments/Prague2025_Ortools.py
@@ -0,0 +1,150 @@
+from tournament import models
+import datetime
+# from tournament.systems.SingleGroupDivisionSystem import SingleGroupDivisionSystem
+# from tournament.systems.TwoGroups8Teams import TwoGroups8Teams
+from tournament.systems.TwoGroups import TwoGroups
+# from tournament.systems.TwoGroups import TwoGroups
+# from tournament.systems.TwoGroups8TeamsCross import TwoGroups8TeamsCross
+# from tournament.systems.TwoGroups8TeamsMiddle import TwoGroups8TeamsMiddle
+# from tournament.systems.FourGroups12Teams import FourGroups12Teams
+# from tournament.systems.FourGroups16Teams import FourGroups16Teams
+# from tournament.systems.ThreeGroups15Teams import ThreeGroups15Teams
+#from tournament.systems.FourGroups15Teams import FourGroups15Teams
+
+from tournament.TournamentScheduler import (
+    TournamentScheduler,
+    TournamentSchedulerDataframeOptimizerOrtools,
+)
+
+import pytz
+
+# run in shell:
+# docker compose exec tournament_scheduler python /srv/django/manage.py shell -c 'from tournament.tournaments import Prague2025_Ortools'
+
+# turnaj
+tslug = "PIT2025_TEST_ORTOOLS_05"
+tname = "PIT 2025 TEST (OR-Tools)"
+prague2025 = models.Tournament(name = tname, slug = tslug)
+prague2025.save()
+print(prague2025)
+####################################################
+# men 1
+
+Men1_system = TwoGroups(prague2025,'Men Elite','MenElite',14)
+Men1_system.division.CreateTeams(
+    [
+        "RKV Berlin",
+        "KSV Glauchau",
+        "Kaniow Men A", 
+        "Poznań", 
+        
+        "Kaniow Men B", 
+        "DRC Neuburg",
+        "Prague A", 
+        "VMW Berlin Men",
+
+        "VK Berlin U21", 
+        "UKK Wien", 
+        "Dresden Men A", 
+        "Bremen",
+
+        "Nagymaros",
+        "Katowice Men", 
+    ]
+)
+
+
+####################################################
+# Ladies
+Ladies_system = TwoGroups(prague2025,'Ladies','Ladies',9, semi5_8=True)
+Ladies_system.division.CreateTeams(
+     [
+        "VMW Berlin Women", 
+        "Kaniow Women", 
+        #"MOSW Choszczno Women", 
+        "Katowice Women", 
+        "O40&babies", 
+        "Swiss U21", 
+        "Havelbrüder Women", 
+        "Girls 99", 
+        "Dresden Women", 
+        "Czech Women U21", 
+    ]
+)
+
+####################################################
+# men 2
+Men2_system = TwoGroups(prague2025,'Men 2','Men2',9, semi5_8=True)
+Men2_system.division.CreateTeams(
+    [
+        "WCH Berlin", 
+        "USV Potsdam", 
+        "Leśná Men", 
+        "KWS Ukraine Men A", 
+        #"KWS Ukraine Men B", 
+        "Dresden Men B", 
+        "DobroPtaah", 
+        "Kaniow Men C", 
+        "Czech U21 M", 
+        #"Innsbruck", 
+        "Vidra Old Boys", 
+    ]
+)
+
+####################################################
+# U15
+U15_system = TwoGroups(prague2025,'U15','U15',11, semi5_8=True)
+U15_system.division.CreateTeams(
+    [
+        "Havelbrüder U15", 
+        "VK Berlin U15", 
+        "Kaniow U15", 
+        "Leśná U15", 
+        "Prague U15", 
+        "Wien U15", 
+        "Dresden U15", 
+        "Alytus U15", 
+        "VMW Berlin U15 A", 
+        "VMW Berlin U15 B", 
+        "Hungary U15", 
+    ]
+)
+
+####################################################
+# U12
+U12_system = TwoGroups(prague2025,'U12','U12',8, semi5_8=True)
+U12_system.division.CreateTeams(
+    [
+        "Havelbrüder U12", 
+        "Dresden U12", 
+        "Prague U12", 
+        "DFF Veltrusy", 
+        "Warszawa U12", 
+        "Leśná U12", 
+        "Glauchau U12", 
+        "Hungary U12", 
+    ]
+)
+
+
+# scheduler
+ts = TournamentScheduler(prague2025,5)
+# pridame rozhodci
+ts.AddReferees()
+
+# naplanujeme zapasy pomoci OR-Tools 
+from tournament.scheduler.ortools_test import run_full_test
+result = run_full_test(prague2025.id, num_slots=40, num_pitches=5)
+
+# ziskame zpet dataframe v puvodnim tvaru a vlozime do schedule
+from tournament.scheduler.ortools_build_dataframe import build_slot_pitch_dataframe
+ts.schedule = build_slot_pitch_dataframe(result)
+
+# zalozime zapasy v DB
+ts.Schedule(
+    [
+        (datetime.datetime(2025,5,17,7,30,tzinfo = pytz.utc),datetime.datetime(2025,5,17,19,00,tzinfo = pytz.utc)),
+        (datetime.datetime(2025,5,18,7,30,tzinfo = pytz.utc),datetime.datetime(2025,5,18,23,30,tzinfo = pytz.utc)),
+    ]
+)
+
