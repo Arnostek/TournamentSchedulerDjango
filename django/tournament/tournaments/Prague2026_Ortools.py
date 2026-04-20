b4a08afb90f2d5c236a8cd3484657d5f323082d4
Arnost <chaloua@volny.cz>
2026-04-20 21:27:33 +0200

    tymy bereme z konfigurace

--- django/tournament/tournaments/Prague2026_Ortools.py
+++ django/tournament/tournaments/Prague2026_Ortools.py
@@ -52,126 +52,49 @@ def load_division_configs(config_path=DIVISION_CONFIG_PATH):
 # docker compose exec tournament_scheduler python /srv/django/manage.py shell -c 'from tournament.tournaments import Prague2025_Ortools'
 
 # turnaj
+divisions = load_division_configs()
+
 tslug = "PIT2026_SEEDING_17"
 tname = "PIT 2026 TEST (teams, seeding)"
 prague2026 = models.Tournament(name = tname, slug = tslug)
 prague2026.save()
 print(prague2026)
-####################################################
+
 # men 1
+division_slug = 'MenElite'
+division_config = divisions[division_slug]
+Men1_system = ThreeGroups15Teams(prague2026,division_config['name'],division_slug,15)
+Men1_system.division.CreateTeams(division_config['teams'])
 
-Men1_system = ThreeGroups15Teams(prague2026,'Men Elite','MenElite',15)
-Men1_system.division.CreateTeams(
-    [
-        "RKV Berlin",
-        "Team Poznań",
-        "Kaniow BKT",
-        "DRC Neuburg",
-        "VK Berlin",
-        "KV Nurnberg",
-        "Set Kaniow A",
-        "KSV Glauchau",
-        "Alytus",
-        "KP Praha A",
-        "KCNW U21",
-        "KP Bremen",
-        "POL U21",
-        "KC Kelheim",
-        "Katowice M",
-        # "Singapore M",
-        # "Dresden A",
-    ]
-)
-
-
-####################################################
 # Ladies
-Ladies_system = TwoGroups(prague2026,'Ladies','Ladies',9, semi5_8=True)
-Ladies_system.division.CreateTeams(
-     [
-        "Kaniow W",
-        "KCNW Berlin W",
-        "KSVH W",
-        "Neuburg W",
-        "Czech W",
-        "Czech/German",
-        "Powisle W",
-        "Katowice W",
-        "Bremen W",
-        # "Kalisz W",
-        # "Dresden W",
-    ]
-)
+division_slug = 'Ladies'
+division_config = divisions[division_slug]
+Ladies_system = TwoGroups(prague2026,division_config['name'],division_slug,9, semi5_8=True)
+Ladies_system.division.CreateTeams(division_config['teams'])
 
-####################################################
 # men 2
-Men2_system = TwoGroups8Teams(prague2026,'Men 2','Men2',8)
-Men2_system.division.CreateTeams(
-    [
-        "VMW Berlin Men",
-        "Vidra Old Boys",
-        "Nagymaros Men",
-        # "KTW Kalisz Men",
-        "Potsdam Men",
-        "UKK Wien mix",
-        "Ukraine Men",
-        "Lesna Men",
-        "DobroPtaah",
-        # "Ukraine Men B",
-        # "Dresden B",
-    ]
-)
+division_slug = 'Men2'
+division_config = divisions[division_slug]
+Men2_system = TwoGroups8Teams(prague2026,division_config['name'],division_slug,8)
+Men2_system.division.CreateTeams(division_config['teams'])
 
-
-
-####################################################
 # U18
-U18_system = SingleGroupDivisionSystem(prague2026,'U18','U18',7, final_for=2)
-U18_system.division.CreateTeams(
-    [
-        "Alytus U18",
-        "Czech U18 A",
-        "KCNW U16",
-        "Lesna U18",
-        "Kalisz U18",
-        "Nagymaros U18",
-        "Czech U18 B",
-        # "Dresden U18",
-    ]
-)
+division_slug = 'U18'
+division_config = divisions[division_slug]
+U18_system = SingleGroupDivisionSystem(prague2026,division_config['name'],division_slug,7, final_for=2)
+U18_system.division.CreateTeams(division_config['teams'])
 
-####################################################
 # U14
-U14_system = TwoGroups(prague2026,'U14','U14',9, semi5_8=True)
-U14_system.division.CreateTeams(
-    [
-        "KCNW U14",
-        "Powisle U14",
-        "Glauchau U14",
-        # "VK Berlin U14",
-        "Neuburg U14",
-        "Lesna U14",
-        "Praha U14 M",
-        "Praha U14 W",
-        # "Dresden U14",
-        "SG Franken U14",
-        "Ukraine U14",
-    ]
-)
+division_slug = 'U14'
+division_config = divisions[division_slug]
+U14_system = TwoGroups(prague2026,division_config['name'],division_slug,9, semi5_8=True)
+U14_system.division.CreateTeams(division_config['teams'])
 
-####################################################
 # U12
-U12_system = SingleGroupDivisionSystem(prague2026,'U12','U12',5)
-U12_system.division.CreateTeams(
-    [
-        "KCNW U12",
-        "DFF Veltrusy U12",
-        "Praha U12",
-        "Nagymarosz U12",
-        "Lesna U12",
-        # "Dresden U12",
-    ]
-)
+division_slug = 'U12'
+division_config = divisions[division_slug]
+U12_system = SingleGroupDivisionSystem(prague2026,division_config['name'],division_slug,5)
+U12_system.division.CreateTeams(division_config['teams'])
 
 
 # scheduler
