3689a1c02513a9e16d7dbb0d71ecea73e13b2410
Arnost <chaloua@volny.cz>
2026-04-21 21:41:21 +0200

    prefered pitches in pit 2026

--- django/tournament/tournaments/Prague2026_Ortools.py
+++ django/tournament/tournaments/Prague2026_Ortools.py
@@ -25,48 +25,56 @@ import pytz
 # turnaj
 divisions = load_division_configs("config/prague2026.yaml")
 
-tslug = "PIT2026_SEEDING_17"
+tslug = "PIT2026_SEEDING_22"
 tname = "PIT 2026 TEST (teams, seeding)"
 prague2026 = models.Tournament(name = tname, slug = tslug)
 prague2026.save()
 print(prague2026)
+
+# map division_id -> list of preferred pitch indices (0-based, ordered by preference)
+preferred_pitches = {}
 
 # men 1
 division_slug = 'MenElite'
 division_config = divisions[division_slug]
 Men1_system = ThreeGroups15Teams(prague2026,division_config['name'],division_slug,len(division_config['teams']))
 Men1_system.division.CreateTeams(division_config['teams'])
+preferred_pitches[Men1_system.division.id] = [0, 1, 2, 3, ]
 
 # Ladies
 division_slug = 'Ladies'
 division_config = divisions[division_slug]
 Ladies_system = TwoGroups(prague2026,division_config['name'],division_slug,len(division_config['teams']), semi5_8=True)
 Ladies_system.division.CreateTeams(division_config['teams'])
+preferred_pitches[Ladies_system.division.id] = [1, 0, 2, 3, ]
 
 # men 2
 division_slug = 'Men2'
 division_config = divisions[division_slug]
 Men2_system = TwoGroups8Teams(prague2026,division_config['name'],division_slug,len(division_config['teams']))
 Men2_system.division.CreateTeams(division_config['teams'])
+preferred_pitches[Men2_system.division.id] = [3, 2, 1, 0, 4]
 
 # U18
 division_slug = 'U18'
 division_config = divisions[division_slug]
 U18_system = SingleGroupDivisionSystem(prague2026,division_config['name'],division_slug,len(division_config['teams']), final_for=2)
 U18_system.division.CreateTeams(division_config['teams'])
+preferred_pitches[U18_system.division.id] = [1, 0, 2, 3, ]
 
 # U14
 division_slug = 'U14'
 division_config = divisions[division_slug]
 U14_system = TwoGroups(prague2026,division_config['name'],division_slug,len(division_config['teams']), semi5_8=True)
 U14_system.division.CreateTeams(division_config['teams'])
+preferred_pitches[U14_system.division.id] = [4, 3, 2, 1, 0]
 
 # U12
 division_slug = 'U12'
 division_config = divisions[division_slug]
 U12_system = SingleGroupDivisionSystem(prague2026,division_config['name'],division_slug,len(division_config['teams']))
 U12_system.division.CreateTeams(division_config['teams'])
-
+preferred_pitches[U12_system.division.id] = [4, 3, 2, 1, 0]
 
 # scheduler
 ts = TournamentScheduler(prague2026,5)
@@ -75,7 +83,7 @@ ts.AddReferees()
 
 # naplanujeme zapasy pomoci OR-Tools 
 from tournament.scheduler.ortools_scheduler import ortools_scheduler
-ts.schedule = ortools_scheduler(prague2026.id, num_slots=38, num_pitches=5, buffer_every_slots=7)
+ts.schedule = ortools_scheduler(prague2026.id, num_slots=38, num_pitches=5, buffer_every_slots=7, preferred_pitches=preferred_pitches)
 
 # zalozime zapasy v DB
 ts.Schedule(
